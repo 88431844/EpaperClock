bf3e40e2828a010c803fe1d6e89385ba60a3fa54
miracle <88431844@163.com>
2020-03-10 10:47:17 +0800

    修复开资日不显示时间问题

--- code/c.py
+++ code/c.py
@@ -158,7 +158,7 @@ try:
         else:
             time_draw.text((10, 10), u' ' + time.strftime('%m-%d') + u' 周' + digital_to_chinese(
                 time.strftime('%w')) + u' | 距开资:' + str(moneyDay) + u'天', font=font24, fill=0)
-            time_draw.text((10, 35), time.strftime('%H:%M'), font=font, fill=0)
+        time_draw.text((10, 35), time.strftime('%H:%M'), font=font, fill=0)
         # time_draw.text((10, 25), time.strftime('%H:%M'), font=font, fill=0)
         # time_draw.text((10, 110), u'今:' + temperature + u'°C ' + weather + u' 明:' + tomorrowNightTemp + u'~' + tomorrowDayTemp + u'°C ' + tomorrowWeather + u' 更:' + reportTime, font=font18, fill=0)
         newimage = time_image.crop([10, 10, 120, 150])
